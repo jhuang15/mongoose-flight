4dae1dac7aa06b3646fc9d51ff74cf317426b7d6
jhuang15 <jchuang1020@gmail.com>
2022-04-20 02:46:40 -0400

    Begin part 3 tickets

---
+++ controllers/tickets.js
@@ -0,0 +1,15 @@
+const Ticket = require('../models/ticket');
+const Flight = require('../models/flight');
+
+module.exports = {
+  new: newTicket,
+  create
+};
+
+function create(req, res) {
+  
+}
+
+function newTicket(req, res) {
+  //Ticket.find({}).sort('airport')
+}
